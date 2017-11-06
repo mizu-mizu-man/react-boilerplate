7717818abd159a9fc270ad72ac248b6d3e6df698
Fanta Straus <mizu-mizu@yandex.ru>
2017-11-06 22:49:38 +0300

    Init

---
+++ src/store/configureStore.js
@@ -0,0 +1,16 @@
+import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk';
+import authReducer from '../reducers/auth';
+
+const composeEnchancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+export default () => {
+  const store = createStore(
+    combineReducers({
+      auth: authReducer
+    }),
+    composeEnchancers(applyMiddleware(thunk))
+  );
+
+  return store;
+};
